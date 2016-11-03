7c2d2757c7339d5c68a81d247ea6855399a2ebcc
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-02 22:13:36 -0400

    Convert item.text to item.content
    Use item.text in form
    Use serialize() in item serializer to convert from 'text' to 'content' before sending request

--- app/item/serializer.js
+++ app/item/serializer.js
@@ -3,7 +3,16 @@ import ApplicationSerializer from 'ga-wdi-boston.ember-resources/application/ser
 import DS from 'ember-data';
 
 export default ApplicationSerializer.extend({
+  serialize(snapshot, options) {
+    // switch from text to content on way out
+    let json = this._super(...arguments);
+    console.log('json is', json);
+    json.content = json.text;
+    delete json.text;
+    return json;
+  },
   normalize(model, response) {
+    // switch from content to text on way in
     response.text = response.content;
     delete response.content;
     return this._super(model, response);
